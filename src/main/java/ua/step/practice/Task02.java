37b18d7dc65cfd7e719f4d3ce72d1e79fa2c47d6
shabrat2015 <shabrat2015@gmail.com>
2020-10-04 21:13:57 +0300

    T2 good

--- src/main/java/ua/step/practice/Task02.java
+++ src/main/java/ua/step/practice/Task02.java
@@ -13,14 +13,18 @@ public class Task02 {
     public static void main(String[] args) {
         // TODO: не менять стоки ниже - необходимо для тестирования @see ua.step.homework.TaskTest02
         long seed = 0;// args.length > 0 ? Long.parseLong(args[0]) : LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
-
+ int min=0,max=0;
         // Использовать для генерирования элементов массива
         Random rnd = new Random(seed);
-
+ int mor[]=new int[30];
         // TODO: Пишите код здесь
         for (int i = 0; i < 30; i++) {
-            System.out.print(rnd.nextInt(51+70) - 70); // 0..120 - 70 = -70..+50
-            System.out.print(" ");
+            mor[i]=(rnd.nextInt(51+70) - 70);
         }
+        for (int i = 0; i < 30; i++) {
+            if(max< mor[i]){max= mor[i];}
+            if(min> mor[i]){ min=mor[i];}
+        }
+        System.out.println(min+" "+max);
     }
 }
