a739c231875cfc6e447fe4a027a52e72a276f68b
shabrat2015 <shabrat2015@gmail.com>
2020-10-04 21:09:35 +0300

    T1 good

--- src/main/java/ua/step/practice/Task01.java
+++ src/main/java/ua/step/practice/Task01.java
@@ -10,7 +10,20 @@ package ua.step.practice;
 public class Task01 {
     public static void main(String[] args) {
         // TODO: Пишите код здесь
-
-
+ int mar[]=new int [15];
+        int n0 = 1;
+        int n1 = 1;
+        mar[0]=n0;mar[1]=n1;
+        int n2;
+        for(int i = 2; i < 15; i++){
+            n2=n0+n1;
+            mar[i]=n2;
+            n0=n1;
+            n1=n2;
+        }
+        for(int t=0;t<15;t++)
+        {
+            if(t%2==0) System.out.print(mar[t]+" ");
+        }
     }
 }
