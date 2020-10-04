4713dc2fa1c631c7f6764e40f8ff4342eece6465
shabrat2015 <shabrat2015@gmail.com>
2020-10-04 21:25:27 +0300

    T3 good

--- src/main/java/ua/step/practice/Task03.java
+++ src/main/java/ua/step/practice/Task03.java
@@ -11,13 +11,19 @@ import java.util.Scanner;
 public class Task03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Введите размер массива: ");
         int len = scanner.nextInt();
-
         // TODO: Пишите код здесь
-        int[] arr = new int[len];
+        int[] arr = new int[len]; int y,h=0;
         for (int i = 0; i < arr.length; i++) {
             arr[i] = scanner.nextInt();
         }
+        y=arr[0];
+        for(int t=0;t<arr.length;t++){
+            if(y==arr[t]) h=1;
+            else if(y!=arr[t]) {h=2;break;}
+        }
+        System.out.println(h==1? "Yes":"No");
     }
 }
