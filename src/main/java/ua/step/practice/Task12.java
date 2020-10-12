2514282b76f0d7cf41db84fb5e064c9dace5e08f
shabrat2015 <shabrat2015@gmail.com>
2020-10-12 18:56:06 +0300

    т12 непонимаю , что за адское стражание с описанием

--- src/main/java/ua/step/practice/Task12.java
+++ src/main/java/ua/step/practice/Task12.java
@@ -26,9 +26,30 @@ public class Task12 {
         System.out.print("Введите размернось массива (сколько строк и колонок): ");
         int rows = scanner.nextInt();
         int cols = scanner.nextInt();
-
-        int[][] marr;
+        int tr=0,j=0,k=0;
+        int[][] marr= new int[rows][cols]; int y=cols,u=0;
         // TODO: Пишите код здесь
+        for (int t=0;t<rows;t++)
+        {
+            for (int i = 0; i < cols; i++)
+            {
+                marr[t][i]= tr = scanner.nextInt();
+            }
+        }
+        y = cols;
 
+            for (int i = 0; i < cols; i++) {
+                if (marr[0][i] == marr[1][y - 1]) {
+                    u += 1;
+                }
+                y--;
+            }
+
+        if (u >= rows )
+        { System.out.println("yes");u=0;
+        }
+        else
+        { System.out.println("no");u=0;
+        }
     }
 }
